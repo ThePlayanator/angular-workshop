72128e90a33f804109106668a02e6fd0864fc774
ThePlayanator <saifulimran2407@gmail.com>
2024-11-04 15:24:01 +0800

    Multiple class binding

--- angular-tutorials/bindingdemo/src/app/app.component.ts
+++ angular-tutorials/bindingdemo/src/app/app.component.ts
@@ -10,8 +10,38 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
 
+  // Approach #1: A space-delimited string of class names
+  // Classess are separated by space
+  stringOfClasses = "special safe";
+
+  stringOfDivClasses = "borderStyle1 borderColor1";
+
+  // Approach #2: An  object with class names as the keys and 
+  // truthy or falsy expressions as the values
+  classesToAdd = {
+    large : false,
+    special : this.stringOfClasses.length > 2,
+    danger :  this.alwaysReturnTrue ()
+  };
+
+  classesDivToAdd = {
+    borderWidth2 : false,
+    borderStyle2 : this.stringOfClasses.length > 2,
+    borderColor2 :  this.alwaysReturnTrue ()
+  };
+
+  // Approach #3: An array of class names
+  classesInArray = ['medium','emphasize','safe'];
+
+  classesDivInArray = ['borderColor2', 'borderStyle1', 'borderColor1']
+
+  alwaysReturnTrue () {
+    return true;
+  }
+
   isThisForReal = true;
 
-  isItDangerous = false;
+  isItDangerous = true;
+
 
 }
