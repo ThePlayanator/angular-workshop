745185faa3304a9cb61c2d2288fd4e87ec08b0a3
ThePlayanator <saifulimran2407@gmail.com>
2024-11-05 14:15:51 +0800

    Two-way Binding

--- angular-tutorials/bindingdemo/src/app/app.component.ts
+++ angular-tutorials/bindingdemo/src/app/app.component.ts
@@ -1,82 +1,36 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
 
-  // for controlling dynamic binding of classes
-  isItDangerous = false;
-  isItSpecial = false;
-  isItLarge = false;
+  initialValue = 'cat';
 
-  // for controlling disabled status of green button
-  isDisabled = false;
+  secondBoxContent = '';
+  thirdBoxContent = '';
 
-  // to store contents of first text box
-  firstTextBox = "";
-
-  processFirstBox(hie: HTMLInputElement) {
-    this.firstTextBox = hie.value;
+  constructor() {
+    this.resetTextBoxes();
   }
 
-  imageName = "cat";
-  animalNames = ['cat','dog','horse'];
-
-  processAnimalBox(hie: HTMLInputElement) {
-    for (const name of this.animalNames) {
-      if (hie.value === name)
-        this.imageName = name;
-    }
+  // Convert the content of the third text box into upper case
+  processThirdBox(boxContent: string) {
+    this.thirdBoxContent = boxContent.toLocaleUpperCase();
   }
 
-  totalValue: number =0;
-  complexity: string ='';
-
-  isCosmetic = false;
-  isProduct = false;
-  isOthers = false;
-
-  complexityValues = {
-    Cosmetics: 1,
-    'New Product': 2,
-    Others: 3
-  };
-
-  calculateValue(){
-    this.totalValue=0;
-    let totalCosmetics=0, totalProducts=0, totalOthers=0;
+  resetTextBoxes() {
     
-    if(this.isCosmetic)
-    {
-      totalCosmetics = this.complexityValues.Cosmetics;
-    }
-    if(this.isProduct)
-    {
-      totalProducts = this.complexityValues['New Product'];
-    }
-    if(this.isOthers)
-    {
-      totalOthers = this.complexityValues.Others;
-    }
+    this.secondBoxContent = this.initialValue;
+    this.thirdBoxContent = this.initialValue;
 
-    this.totalValue=totalCosmetics+totalOthers+totalProducts;    
-
-    if (this.totalValue === 0) {
-      this.complexity = 'No categories selected';
-    } else if (this.totalValue <= 2) {
-      this.complexity = 'Low Complexity';
-    } else if (this.totalValue <= 4) {
-      this.complexity = 'Medium Complexity';
-    } else {
-      this.complexity = 'High Complexity';
-    }
   }
-
 
 }
