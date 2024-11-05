eca78c492a3e1e66b87334e33ae67fb7e08db6a9
ThePlayanator <saifulimran2407@gmail.com>
2024-11-05 23:43:36 +0800

    Parent-Child creation

--- angular-tutorials/parentchilddemo/src/app/app.component.ts
+++ angular-tutorials/parentchilddemo/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FirstChildComponent } from './first-child/first-child.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FirstChildComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'parentchilddemo';
 }
