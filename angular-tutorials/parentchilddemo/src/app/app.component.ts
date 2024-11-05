5c83fa1c8a030e1ebc2a8e5d7223702b514f7937
ThePlayanator <saifulimran2407@gmail.com>
2024-11-06 01:52:37 +0800

    Exerrcise - Data transfer child to parent

--- angular-tutorials/parentchilddemo/src/app/app.component.ts
+++ angular-tutorials/parentchilddemo/src/app/app.component.ts
@@ -1,19 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FirstChildComponent } from './first-child/first-child.component';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FirstChildComponent, FormsModule],
+  imports: [RouterOutlet, FirstChildComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 export class AppComponent {
-  
+
   parentCounter = 0;
+
+  childText = '';
+  selectedColour: string=''
 
   incCounter() {
     this.parentCounter++;
@@ -23,7 +25,13 @@ export class AppComponent {
     this.parentCounter--;
   }
 
-  // Q1
-  name: string ='';
+  processChangeFromChild(val: string) {
+    this.childText = val;
+  }
+
+   // New event handler for color selection
+   processColourChange(colour: string) {
+    this.selectedColour = colour; // Store the selected color
+}
 
 }
