581e7c5633834a4f342463ea2c1b37e4b48c357e
ThePlayanator <saifulimran2407@gmail.com>
2024-11-07 14:37:35 +0800

    6.6 - @for and accessing index of item in collection

--- angular-tutorials/directivesdemo/src/app/app.component.ts
+++ angular-tutorials/directivesdemo/src/app/app.component.ts
@@ -3,32 +3,39 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { Employee } from './Employee';
-import { EmployeeComponent } from './employee/employee.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, FormsModule, EmployeeComponent],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 export class AppComponent {
 
-  animals: string[] = ['cat','dog','mouse','horse'];
-
   employees: Employee[] = [];
 
-  // initializing array with 4 new objects
+  message = '';
+
+  // initializing array with 8 new objects
   constructor() {
     this.employees.push(new Employee("Peter",42,true));
     this.employees.push(new Employee("Jane",22,false));
     this.employees.push(new Employee("Hilary",36,true));
     this.employees.push(new Employee("Scot",27,false));
+    this.employees.push(new Employee("Noah",50,true));
+    this.employees.push(new Employee("Olivia",20,false));
+    this.employees.push(new Employee("Amelia",60,true));
+    this.employees.push(new Employee("William",29,false));
   }
 
-  removeEmployee() {
-    this.employees.pop();
+  showButtonInfo(pos: number) {
+    this.message = `You clicked button : ${pos} for ${this.employees[pos].name}`; 
+  }
+
+  changeName(buttonPos: string, newVal: string) {
+    this.employees[parseInt(buttonPos)].name = newVal;
   }
 
 
