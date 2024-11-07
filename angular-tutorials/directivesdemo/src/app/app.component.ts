f623daf808aef4ea7e4b08ce3de1954dba54671c
ThePlayanator <saifulimran2407@gmail.com>
2024-11-07 09:01:04 +0800

    Tutorials - ngFor

--- angular-tutorials/directivesdemo/src/app/app.component.ts
+++ angular-tutorials/directivesdemo/src/app/app.component.ts
@@ -1,36 +1,31 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { Employee } from './Employee';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 export class AppComponent {
 
-  styleTheText = {
-    'font-size' : '30px',
-    'font-style' : 'italic',
-    'color' : 'green'
+  animals: string[] = ['cat','dog','mouse','horse'];
+
+  employees: Employee[] = [];
+
+  // initializing array with 4 new objects
+  constructor() {
+    this.employees.push(new Employee("Peter",42,true));
+    this.employees.push(new Employee("Jane",22,false));
+    this.employees.push(new Employee("Hilary",36,true));
+    this.employees.push(new Employee("Scot",27,false));
   }
 
-  // No error checking performed
-  // Make sure you enter a valid value
-  changeColor(val: string) {
-    console.log("New color : ", val);
-    this.styleTheText.color = val;
-  }
-
-  // No error checking performed
-  // Make sure you enter a valid value
-  changeFontSize(val: string) {
-    console.log("New font size : ", val);
-    this.styleTheText['font-size'] = val + 'px';
-  }
 
 
 }
